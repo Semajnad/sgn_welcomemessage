e87fe2a2787c93f69617ad76d6ff57da9040029b
Daniel Few <danieljamesfew@gmail.com>
2014-05-24 15:49:32 +0100

    Added client and server files
    Added cl_init.lua and sv_init.lua

---
+++ lua/autorun/client/cl_init.lua
@@ -0,0 +1,49 @@
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+
+net.Receive( "sendToPlayer", function()
+    local TOP_MESSAGE_TEXT = net.ReadString()
+    local BOTTOM_MESSAGE_TEXT = net.ReadString()
+    drawMessage( TOP_MESSAGE_TEXT, BOTTOM_MESSAGE_TEXT, TOP_MESSAGE_COLOR_VALUES, BOTTOM_MESSAGE_COLOR_VALUES )
+end)
+
+surface.CreateFont( "topMessageText", {
+	font = "Colaborate-Regular",
+	size = 40,
+	weight = 500,
+	antialias = true,
+	shadow = true,
+} )
+surface.CreateFont( "bottomMessageText", {
+	font = "Colaborate-Regular",
+	size = 55,
+	weight = 500,
+	antialias = true,
+	shadow = true,
+} )
+
+function drawMessage( TOP_MESSAGE_TEXT, BOTTOM_MESSAGE_TEXT, TOP_MESSAGE_COLOR_VALUES, BOTTOM_MESSAGE_COLOR_VALUES )
+
+    local topMessageText = vgui.Create( "DLabel" )
+    topMessageText:SetFont( "topMessageText" )
+    topMessageText:SetText( TOP_MESSAGE_TEXT )
+    topMessageText:SizeToContents()
+    topMessageText:SetContentAlignment( 5 )
+    local topMessageTextX, topMessageTextY = topMessageText:GetSize()
+    topMessageText:SetPos( ( 0-topMessageTextX ), (ScrH()/4) )
+    topMessageText:SetTextColor( Color( 255, 255, 255, 255 ) )
+    topMessageText:MoveTo( (ScrW()/2)-(topMessageTextX/2), (ScrH()/4), 0.1, 15, 1 )
+    topMessageText:AlphaTo( 0, 1, 20 )
+    
+    local bottomMessageText = vgui.Create( "DLabel" )
+    bottomMessageText:SetFont( "bottomMessageText" )
+    bottomMessageText:SetText( BOTTOM_MESSAGE_TEXT )
+    bottomMessageText:SizeToContents()
+    bottomMessageText:SetContentAlignment( 5 )
+    local bottomMessageTextX, bottomMessageTextY = bottomMessageText:GetSize()
+    bottomMessageText:SetPos( ScrW(), (ScrH()/4)+50 )
+    bottomMessageText:SetTextColor( Color( 255, 255, 255, 255 ) )
+    bottomMessageText:MoveTo( (ScrW()/2)-(bottomMessageTextX/2), (ScrH()/4)+30, 0.1, 15.3, 1 )
+    bottomMessageText:AlphaTo( 0, 1, 20 )
+end
---
+++ lua/autorun/server/sv_init.lua
@@ -0,0 +1,62 @@
+//Configuration Options:
+
+//Is the add-on enabled?
+//	true / false
+PLUGIN_ENABLED = true
+
+//Do you want the message to also appear in the players chat box?
+//	true / false
+PRINT_IN_CHAT = true
+
+//Do you want others to get a notification telling them a player has connected?
+//	true / false
+TELL_ALL = true
+
+//What message to do you want to display to the connecting user?
+//  Change [server_name] to your servers name, or re-arrange the string to suit your needs! If you need help, just let me know!
+//  $playername = players garry's mod nickname / display name
+//  $playersteamid = players steam ID
+TOP_MESSAGE_TEXT = "Welcome $playername with steamID $playersteamid to "
+BOTTOM_MESSAGE_TEXT = "Sleepless Gaming Network"
+
+//Top and bottom message font sizes
+TOP_LINE_SIZE = 40
+BOTTOM_LINE_SIZE = 55
+
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+//DO NOT TOUCH ANYTHING PAST THIS LINE//
+
+resource.AddFile( "resource/fonts/ColabReg.ttf" )
+
+if PLUGIN_ENABLED == true then
+    PLUGIN_ENABLED_STATUS = "enabled"
+else
+    PLUGIN_ENABLED_STATUS = "disabled"
+end
+print( "Player Login Message has started and is " .. PLUGIN_ENABLED_STATUS )
+util.AddNetworkString( "sendToPlayer" )
+
+function PlayerJoinTeam( ply )
+	if PLUGIN_ENABLED == true then
+		playerNick = ply:Nick()
+        playerSteamID = ply:SteamID()
+        TOP_MESSAGE_TEXT = string.gsub( TOP_MESSAGE_TEXT, "$playername", playerNick )
+        TOP_MESSAGE_TEXT = string.gsub( TOP_MESSAGE_TEXT, "$playersteamid", playerSteamID )
+        BOTTOM_MESSAGE_TEXT = string.gsub( BOTTOM_MESSAGE_TEXT, "$playername", playerNick )
+        BOTTOM_MESSAGE_TEXT = string.gsub( BOTTOM_MESSAGE_TEXT, "$playersteamid", playerSteamID )
+		net.Start( "sendToPlayer" )
+			net.WriteString( TOP_MESSAGE_TEXT )
+            net.WriteString( BOTTOM_MESSAGE_TEXT )
+		net.Send( ply )
+		if PRINT_IN_CHAT == true then
+			ply:ChatPrint( TOP_MESSAGE_TEXT .. BOTTOM_MESSAGE_TEXT )
+		end
+	end
+end
+hook.Add( "PlayerJoinTeam", "playerInitialSpawn", PlayerJoinTeam )
+
+function fPlayerDisconnect( ply )
+    return false
+end
+hook.Add( "PlayerDisconnected", "playerdisconnected", fPlayerDisconnect )
